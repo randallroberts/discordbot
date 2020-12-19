b689e595dd0f3f8eff073bab0cfe14a778bac3c9
Randall Roberts <randall@randallroberts.ca>
2020-12-19 17:52:48 -0500

    Help command

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const goodBot = require('./data/goodBot.json');
+const commandList = require('./data/commands.json');
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -11,6 +12,27 @@ client.on('ready', () => {
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'good bot') {
     msg.channel.send(goodBot['answers'][Math.floor(Math.random() * (goodBot['answers'].length))]);
+  }
+
+  const prefix = commandList['prefix'];
+
+  //commands all require a ! in front, otherwise exit the event listener
+  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+  const args = msg.content.slice(prefix.length).trim().split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === 'help') {
+    let response = 'Looks like you need a hand! Here\'s everything I know how to do:\n\n';
+    commandList["commands"].forEach(item => {
+      response += prefix + item["command"] + (!!item["params"].trim() ? ': ' + item["params"] : '') + '\n';
+      response += item['desc'] + '\n\n';
+    })
+    // if (!args.length) {
+    //   return msg.channel.send(`You didn't provide any arguments, ${msg.author}!`);
+    // }
+  
+    msg.channel.send(response);
   }
 });
 

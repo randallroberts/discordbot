832583b9df76d3842b1b205e3e08b228b0f9a4d1
Randall Roberts <randall@randallroberts.ca>
2020-12-20 15:16:10 -0500

    Added addGoodBot cmd

--- commands/addGoodBot.js
+++ commands/addGoodBot.js
@@ -1,8 +1,22 @@
+const fs = require('fs');
+let goodBot = require('../data/goodBot.json');
+
 module.exports = {
-    name: 'addGoodBot',
-    usage: '<url>',
+    name: 'addgoodbot',
+    usage: '[url]',
 	description: 'Add a giphy response to the Good Bot command',
 	execute(message, args) {
-		message.channel.send('Pong.');
+        if (!Array.isArray(args) || args.length !== 1) throw 'Incorrect parameters';
+
+        const url = args[0].toLowerCase();
+        goodBot['answers'].push(url);
+        fs.writeFile("./data/goodBot.json", JSON.stringify(goodBot), (err, data) => {
+            if(err) {
+                message.channel.send('I couldn\'t add this. :\'( ');
+                console.error(`Couldn't write ${url} to goodBot.json`);
+            } else {
+                message.channel.send('Oh boy, oh boy, oh boy!');
+            }
+        });
 	},
 };

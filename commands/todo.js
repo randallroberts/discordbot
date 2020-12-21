764cb06c06d28ae1795995efb5ec4fd7e80d18d3
Randall Roberts <randall@randallroberts.ca>
2020-12-20 20:53:52 -0500

    Todo list added for visibility

---
+++ commands/todo.js
@@ -0,0 +1,16 @@
+let goodBot = require('../data/goodBot.json');
+
+module.exports = {
+    name: 'todo',
+    usage: '',
+    description: 'What is Randall going to add next?',
+    cooldown: 2,
+	execute(message, args) {
+        let response = '* Create Channel (require 4+ votes for, by counting emojis)\n';
+        response += '* Anonymous feedback, possibly added to this to-do list (Can be done by PM for privacy)\n';
+        response += '* Test DM disallow (Hal gif loads correctly?)\n';
+        response += '* Test cooldown default vs specific cooldowns\n';
+
+		message.channel.send(response);
+	},
+};

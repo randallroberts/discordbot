62af4a97ac02ea15ad1ce81003c7bfe0de89cc7a
Randall Roberts <randall@randallroberts.ca>
2020-12-20 20:47:53 -0500

    Create and Delete role options added

---
+++ commands/createRole.js
@@ -0,0 +1,24 @@
+module.exports = {
+	name: 'createrole',
+    description: 'Create a new role (no spaces)',
+    usage: '[role]',
+	execute(message, args) {
+        if (!Array.isArray(args) || args.length !== 1) throw 'Incorrect parameters';
+
+        try {
+            message.guild.roles.create({
+                data: {
+                    name: args[0],
+                    color: "grey",
+                },
+                reason: args[0],
+            })
+        }
+        catch (error) {
+            console.error(error);
+            return message.channel.send(`I wasn't able to create ${args[0]}!`);
+        }
+
+        message.channel.send(`${args[0]} was created.`);
+	},
+};
---
+++ commands/deleteRole.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: 'deleterole',
+    description: 'Delete a role (remove all members first)',
+    usage: '[@role]',
+	execute(message, args) {
+        const role = message.mentions.roles.first();
+        if (!role) throw 'Incorrect parameters';
+
+        if (role.members.size > 0) return message.channel.send("There are still users in this role. " + ((role.members.size > 1) ? "Are " : "Is ") + role.members.map(m=>m.user.tag).join(', ') + " ok with this role being deleted? If so, remove them first.");
+        role.delete('The role needed to go')
+        .then(deleted => message.channel.send(`${role} was BALEETED!`))
+        .catch(console.error);
+	},
+};

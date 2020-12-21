40e5ee8bc59f213ab2dd318db78dc5295c9b7d85
Randall Roberts <randall@randallroberts.ca>
2020-12-20 20:08:34 -0500

    addRole created

---
+++ commands/addRole.js
@@ -0,0 +1,21 @@
+module.exports = {
+	name: 'addrole',
+    description: 'Add a user to a role',
+    usage: '[@role] [@user]',
+	execute(message, args) {
+        const user = message.mentions.members.first();
+        const role = message.mentions.roles.first();
+        
+        if (!user || !role) throw 'Incorrect parameters';
+
+        try {
+            user.roles.add(role);
+        }
+        catch (error) {
+            console.error(error);
+            return message.channel.send(`I wasn't able to assign ${user.user.username} to ${role}!`);
+        }
+
+        message.channel.send(`User ${user.user.username} was added to ${role}!`);
+	},
+};
